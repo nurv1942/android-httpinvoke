213e9ab2ffee98f3482cccbef7c529f48b855690
wsh <wsh@json>
2015-03-03 15:29:41 +0800

    New Project

---
+++ android-httpinvoke/src/com/example/android_httpinvoke/TestAndroidHttpInvoke.java
@@ -0,0 +1,15 @@
+package com.example.android_httpinvoke;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TestAndroidHttpInvoke extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test_android_http_invoke);
+	}
+}
